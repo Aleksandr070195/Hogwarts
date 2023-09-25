3304332b5d972f287552a6fa07b13b3d670fb116
Aleksandr <07_sanek_95@mail.ru>
2023-09-25 15:43:27 +0300

    /

--- src/main/java/com/example/hogwarts/model/Student.java
+++ src/main/java/com/example/hogwarts/model/Student.java
@@ -1,11 +1,22 @@
 package com.example.hogwarts.model;
 
+import nonapi.io.github.classgraph.json.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.util.Objects;
 
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private int age;
+
+    public Student() {
+    }
 
     public Student(long id, String name, int age) {
         this.id = id;
--- src/main/java/com/example/hogwarts/service/FacultyService.java
+++ src/main/java/com/example/hogwarts/service/FacultyService.java
@@ -1,57 +1,47 @@
 package com.example.hogwarts.service;
 
-import com.example.hogwarts.exception.FacultyAlreadyExistsException;
 import com.example.hogwarts.exception.FacultyNotFoundException;
 import com.example.hogwarts.model.Faculty;
+import com.example.hogwarts.repository.FacultyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
-    private static Long COUNTER = 0L;
-    private final Map<Long, Faculty> storage = new HashMap<>();
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
 
     public Faculty create(Faculty faculty) {
-        Long id = faculty.getId();
-        if (id != null && storage.containsKey(id)) {
-            throw new FacultyAlreadyExistsException();
-        }
-        Long nextId = COUNTER++;
-        faculty.setId(nextId);
-        storage.put(nextId, faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     public Faculty update(Long id, Faculty faculty) {
-        if (!storage.containsKey(id)) {
-            throw new FacultyNotFoundException();
-        }
-        storage.put(id, faculty);
-        return faculty;
+        Faculty existingFaculty = facultyRepository.findById(id).orElseThrow(FacultyNotFoundException::new);
+        existingFaculty.setColor(faculty.getColor());
+        existingFaculty.setName(faculty.getName());
+        return facultyRepository.save(existingFaculty);
     }
 
     public Faculty getById(Long id) {
-        return storage.get(id);
+        return facultyRepository.findById(id).orElseThrow(FacultyNotFoundException::new);
     }
 
     public Collection<Faculty> getAll() {
-        return storage.values();
+        return facultyRepository.findAll();
     }
 
     public Faculty remove(Long id) {
-        if (!storage.containsKey(id)) {
-            throw new FacultyNotFoundException();
-        }
-        return storage.remove(id);
+        Faculty faculty = facultyRepository.findById(id).orElseThrow(FacultyNotFoundException::new);
+        facultyRepository.delete(faculty);
+        return faculty;
     }
 
     public Collection<Faculty> getAllByColor(String color) {
-        return storage.values().stream()
-                .filter(f -> f.getColor().equalsIgnoreCase(color))
-                .collect(Collectors.toList());
+        return facultyRepository.findAllByColor(color);
     }
 }
